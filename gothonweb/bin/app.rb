951e6886709fcf44bad75c4bd56bdcf578e71198
Juan Andrew _test_ <juandreww@outlook.com>
2022-11-22 08:31:44 +0700

    asdfasdf

--- gothonweb/bin/app.rb
+++ gothonweb/bin/app.rb
@@ -2,14 +2,15 @@ require 'sinatra'
 
 set :port, 8080
 set :static, true
-set :public_folder, "static"
-set :views, "views"
+set :public_folder, 'static'
+set :views, 'views'
 
 get '/' do
   return 'Hello world'
 end
 
 get '/hello/' do
-  greeting = params[:greeting] || "Hi There"
-  erb :index, :locals => {'greeting' => greeting}
+  greeting = params[:greeting] || 'Hi There'
+  name = params[:name] || 'Nobody'
+  erb :index, :locals => {'greeting' => greeting, 'name' => name}
 end
