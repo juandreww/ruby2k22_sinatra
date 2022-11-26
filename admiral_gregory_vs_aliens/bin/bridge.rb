ddf7c461b31812cbc42168267b006fc6ae9bf822
Juan Andrew _test_ <juandreww@outlook.com>
2022-11-26 16:43:01 +0700

    refresh bridge

--- admiral_gregory_vs_aliens/bin/bridge.rb
+++ admiral_gregory_vs_aliens/bin/bridge.rb
@@ -4,21 +4,37 @@ require_relative 'exit_room'
 
 # Bridge
 class Bridge < Spaceship
+  # rubocop:disable Metrics/MethodLength
   def initialize
     super
 
     @new_alien = Alien.new
+    @necromancer = @new_alien.necromancer
     @new_hero = Hero.new
     @new_hero.allow_weapon = true
+    @name = 'Bridge'
+    @description = "Nice try. The color that dont care is 'Biru Donker!'
+
+  The door is opening.. Here your last enemy is standing
+  .
+  .
+  .
+  .
+  .
+
+
+  He meets an Alien with type Necromancer. Due to its intelligence, you cannot use the same move repeatedly.
+  Approaching slowly yet with intent to kill.
+
+  necromancer's HP is now #{necromancer}
+  What the hero should do? (punch/ kick/ headbutt/ excalibur/ muramasa)"
   end
+  # rubocop:enable Metrics/MethodLength
+
+  attr_reader :description, :name
 
   # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def room
-    puts "He meets an Alien with type Necromancer. Due to its intelligence, you cannot use the same move repeatedly.
-    Approaching slowly yet with intent to kill."
-    puts
-
-    necromancer = @new_alien.necromancer
     damage = 0
     previous_move = ''
 
@@ -56,15 +72,15 @@ class Bridge < Spaceship
 
     damage = 0
     case act
-    when 'punch' || 'p'
+    when 'punch'
       damage = @new_hero.punch
-    when 'kick' || 'k'
+    when 'kick'
       damage = @new_hero.kick
-    when 'headbutt' || 'h'
+    when 'headbutt'
       damage = @new_hero.headbutt
-    when 'excalibur' || 'e'
+    when 'excalibur'
       damage = @new_hero.excalibur_attack
-    when 'muramasa' || 'm'
+    when 'muramasa'
       damage = @new_hero.muramasa_rifle_shot
     else
       puts 'Don\'t understand the command. Type again'
@@ -74,4 +90,8 @@ class Bridge < Spaceship
     [damage, next_move]
   end
   # rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+  def damage_dealt_by_action(action, allow_using_weapon)
+    
+  end
 end
